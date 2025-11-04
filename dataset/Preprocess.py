035f130e811dd1f5553703f7ecaf45f3528a3935
Samuele Carrea <s334035@studenti.polito.it>
2025-11-04 18:08:12 +0100

    datatset operations

--- dataset/Preprocess.py
+++ dataset/Preprocess.py
@@ -5,21 +5,6 @@ import os
 import shutil
 
 def Preprocess():
-    # Download and extract Tiny ImageNet dataset if not already present
-    if not os.path.exists('tiny-imagenet'):
-        os.system('wget http://cs231n.stanford.edu/tiny-imagenet-200.zip')
-        os.system('unzip tiny-imagenet-200.zip -d tiny-imagenet')
-
-    # Rearrange validation images into class-specific folders
-    with open('tiny-imagenet/tiny-imagenet-200/val/val_annotations.txt') as f:
-        for line in f:
-            fn, cls, *_ = line.split('\t')
-            os.makedirs(f'tiny-imagenet/tiny-imagenet-200/val/{cls}', exist_ok=True)
-
-        shutil.copyfile(f'tiny-imagenet/tiny-imagenet-200/val/images/{fn}', f'tiny-imagenet/tiny-imagenet-200/val/{cls}/{fn}')
-
-        shutil.rmtree('tiny-imagenet/tiny-imagenet-200/val/images')
-
     # Define the transformations for the training and validation datasets
 
     train_transform = T.Compose([
---
+++ utils/download_dataset.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+
+# Download and extract Tiny ImageNet dataset if not already present
+if not os.path.exists('data/tiny-imagenet'):
+    os.system('wget http://cs231n.stanford.edu/tiny-imagenet-200.zip')
+    os.system('unzip tiny-imagenet-200.zip -d data/tiny-imagenet')
+
+# Rearrange validation images into class-specific folders
+with open('data/tiny-imagenet/tiny-imagenet-200/val/val_annotations.txt') as f:
+    for line in f:
+        fn, cls, *_ = line.split('\t')
+        os.makedirs(f'data/tiny-imagenet/tiny-imagenet-200/val/{cls}', exist_ok=True)
+
+    shutil.copyfile(f'data/tiny-imagenet/tiny-imagenet-200/val/images/{fn}', f'data/tiny-imagenet/tiny-imagenet-200/val/{cls}/{fn}')
+
+    shutil.rmtree('data/tiny-imagenet/tiny-imagenet-200/val/images')
